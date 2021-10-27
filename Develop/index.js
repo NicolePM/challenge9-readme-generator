10459f606b5178ae7cf7682bd1b0030e67964576
NicolePM <chanelmitchell2006@hotmail.com>
2021-10-26 20:34:25 -0500

    Build questions for md readme file

--- Develop/index.js
+++ Develop/index.js
@@ -2,15 +2,74 @@
 const inquirer=require("inquirer")
 const fs=require("fs")
 const generateMarkdown=require("./utils/generateMarkdown")
+const path = require('path');
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+  .prompt([
+    { 
+      type:"input",
+      message: "What is the Title for your application or project?",
+      name: "title",  
+         },
+    { 
+      type: "input",
+      message: "Provide an in depth Description of your application or project?",
+      name: "description",  
+         },
+    {
+       type: "input",
+       message: "How do you Install or Load your application or project?",
+       name: "installation",
+    },
+    {
+      type: "input",
+      message: "What is the website of your application or project?",
+      name: "website",
+   },
+   {
+    type:"editor",
+    message: "Built with:",
+    name: "software",
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+    {
+        type: "input",
+        message: "What is the purpose of your application or project?",
+        name: "usage",
+    },
+    {
+        type:"input",
+        name:"credits",
+        message:"Who are your collaborators?"
+    },
+    {
+        type:"list",
+        message: "What License did you use for this application or project?",
+        choices: ["MIT", "GNU", "Apache", "BSD"],
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "How can people Contribute to your project?",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "Badges?",
+        name: "badge"
+    },
+    
+    {
+        type: "input",
+        message: "What is your email address where users and contributors can send questions?",
+        name: "email"
+    },
+  ])
+  .then((response) => {
+    return fs.writeFileSync(path.join (process.cwd(), "README.md"), generate(response));
+  });
 
-// Function call to initialize app
+
 init();
